0fe3199a7049cf9ce87b4fd8b0e154af65177fcd
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-09 19:11:57 -0300

    Add VitaRenderer class for MetalKit rendering

---
+++ VitaGo/VitaRenderer.swift
@@ -0,0 +1,12 @@
+import Foundation
+import MetalKit
+
+class VitaRenderer: NSObject, MTKViewDelegate {
+    
+    func draw(in view: MTKView) {
+        // Renderização futura
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+}
