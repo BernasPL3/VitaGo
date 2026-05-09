e3e1f66dfb9a0efe7ce7db6f79561f7f7ca1e722
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-09 19:10:51 -0300

    Create EmulatorCore.swift

---
+++ VitaGo/EmulatorCore.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class EmulatorCore {
+    
+    func start() {
+        print("PSVita Emulator Started")
+    }
+    
+    func loadGame(path: String) {
+        print("Loading game: \(path)")
+    }
+}
